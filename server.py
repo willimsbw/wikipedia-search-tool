2e90d5898644c4de228835c722215a4c2e62a0f9
Bryan <willimsbw@gmail.com>
2018-05-04 16:54:14 -0400

    Changed from simplehtttprequesthandler to basehttprequesthandler

--- server.py
+++ server.py
@@ -3,11 +3,16 @@ import socketserver
 import os
 
 port = int(os.environ.get('PORT', 8000)) # Use 8000 if no PORT variable
+content = open('index.html')
 
-Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map.update({
-    '.html': 'text/html',
-});
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Root path. Send the form.
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        self.wfile.write(content.encode())
 
 httpd = socketserver.TCPServer(("", port), Handler)
 
